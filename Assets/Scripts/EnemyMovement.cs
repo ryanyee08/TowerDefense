0fdcea7862d4de1ae50b0c52d251ec7ac4deacd9
ryanyee08 <ryanyee08@gmail.com>
2022-02-16 16:12:55 -0500

    Implemented Waypoint Based Enemy Movement
    Implemented logic where the enemy moves between different waypoints until finally reaching the last waypoint

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* ================
+ * Enemy Movement
+ * ================
+ * <Summary>
+ * This script handles the movement of the enemy objects.
+ * When the enemy is spawned it will load navigational data from a level manager
+ * Then it will proceed to follow the waypoints all the way to the goal
+ * </Summary>
+ */
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private LevelManager LevelManager;
+    [SerializeField] int EnemyMovementSpeed = 1;
+    private int waypointIndex = 0;
+
+    private void Update()
+    {
+        // Move the enemy towards the currently targeted waypoint every frame
+        transform.position = Vector3.MoveTowards(transform.position, LevelManager.waypoints[waypointIndex].position, EnemyMovementSpeed * Time.deltaTime);
+        
+        // Check to see if the object has reached a waypoint
+        if (transform.position == LevelManager.waypoints[waypointIndex].position)
+        {
+            // If this is the final waypoint then destroy the object
+            if (waypointIndex == LevelManager.waypoints.Length - 1)
+            {
+                Destroy(gameObject);
+            }
+
+            // If it is not the final waypoint go to the next one
+            waypointIndex++;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*<Summary>
+ *Level Manager stores all data and manages the current level 
+ *</Summary>*/
+public class LevelManager : MonoBehaviour
+{
+    // Waypoints stores the location of each waypoint we want the enemy to move to as it traverses the level
+    public Transform[] waypoints;
+
+}
